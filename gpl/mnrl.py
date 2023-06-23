c34ba9bbf52d2c161366e67394eb2cf19cd44216
Reneeth Krishna MG <101785701+qberg@users.noreply.github.com>
2023-06-23 13:29:34 +0530

    Update mnrl.py

--- gpl/mnrl.py
+++ gpl/mnrl.py
@@ -13,12 +13,9 @@ def mnrl(
     qgen_prefix="qgen",
 ):
     prefix = qgen_prefix
-    #### Training on Generated Queries ####
     corpus, gen_queries, gen_qrels = GenericDataLoader(data_path, prefix=prefix).load(
         split="train"
     )
-
-    #### Provide any HuggingFace model and fine-tune from scratch
     model_name = base_ckpt
     word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
     pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
@@ -27,12 +24,10 @@ def mnrl(
     #### Provide any sentence-transformers model path
     retriever = TrainRetriever(model=model, batch_size=75)
 
-    #### Prepare training samples
     train_samples = retriever.load_train(corpus, gen_queries, gen_qrels)
     train_dataloader = retriever.prepare_train(train_samples, shuffle=True)
     train_loss = losses.MultipleNegativesRankingLoss(model=retriever.model)
 
-    #### Provide model save path
     model_save_path = output_dir
     os.makedirs(model_save_path, exist_ok=True)
 
